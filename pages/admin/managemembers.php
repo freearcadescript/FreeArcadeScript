a3ef46dcdd921baded958812ce1a80f359a5d700
Dwayne <thearcadenetwork@gmail.com>
2014-10-10 15:48:23 -0500

    managemebers
    revert page fix

--- pages/admin/managemembers.php
+++ pages/admin/managemembers.php
@@ -56,6 +56,7 @@ if($seo_on == 1){
 }else{
 	$urlp = ''.$domain.'/index.php?action=showprofile&profile='.$userid ;
 }
+
 echo'<tbody>
 	<tr>
 		<td width="50px">'.$ir['userid'].'</td>
@@ -67,12 +68,35 @@ echo'<tbody>
 }
 
 echo '</table>
-<div class="page-box">'.$totalres.' user(s) - Page '.$show.' of '.$totalpages.' - ';
-for($i = 1; $i <= $totalpages; $i++){
-	if($show == $i){
-		echo '<a href="'.$domain.'/index.php?action=admin&case=managemembers&page='.$i.'" class="page-select">'.$i.'</a>&nbsp; ';
-	}else{
-		echo '<a href="'.$domain.'/index.php?action=admin&case=managemembers&page='.$i.'" class="page">'.$i.'</a>&nbsp; ';
+<div class="page-box">'
+.$totalres.' game(s) - Page '.$show.' of '.$totalpages;
+$pre = $show - '1';
+$ne = $show + '1';
+
+$previous = ''.$domain.'/index.php?action=admin&case=managemembers&page='.$pre.'';
+$next = ''.$domain.'/index.php?action=admin&case=managemembers&page='.$ne.'';
+
+if ($totalpages > '1'){
+	echo' - ';
+	if ($show > '1'){
+		echo '<a href="'.$previous.'" class="page">Previous</a>';
+	}
+	for($i = 1; $i <= $totalpages; $i++){
+		if($show - $i < '4' || $totalpages - $i < '7'){
+			if($i - $show < '4' || $i < '8'){
+				
+				$urk = ''.$domain.'/index.php?action=admin&case=managemembers&page='.$i.'';
+				
+				if($show == $i){
+					echo '<a href="'.$urk.'" class="page-select">'.$i.'</a>';
+				}else{
+					echo '<a href="'.$urk.'" class="page">'.$i.'</a>';
+				}
+			}
+		}
+	}
+	if ($show < $totalpages){
+		echo '<a href="'.$next.'" class="page">Next</a>';
 	}
 }
 echo '</div>';
